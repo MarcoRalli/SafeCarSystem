9e1126d5885f78f7d0d986af5b0d537fa8e1432e
MarcoRalli <marco_ralli@outlook.com>
2023-02-16 10:19:12 +0100

    project web cam

--- classification.py
+++ classification.py
@@ -30,6 +30,7 @@ def classification(model, image):
     class_prob=model.predict(input_arr,batch_size=1)
 
     res = np.where(class_prob == np.max(class_prob))[1]
+    print(res)
     if(res == 0):
         return("Phone")
     elif(res == 1 ):
--- main.py
+++ main.py
@@ -11,26 +11,59 @@ from PIL import Image
 import cv2
 import os
 import classification
-
-
+from playsound import playsound
 
 
 if __name__ == "__main__":
 
-    print("System is going to start----->\n")
-    vgg_model_dropout = None
-    #while(vgg_model_dropout != None):
-    print(".......\n")
-    flag = 0
     count = 0
+    num_image = 0
+    flag_safe = 0
+    flag_dist = 0
+    flag_phone = 0
+
+    print("System is going to start----->\n")
+    
+
+    print(".......\n")
+
+    mac_cam = cv2.VideoCapture(0)
     vgg_model = tf.keras.models.load_model("mode/CNN_pretrained/CNN_VGG16_Dropout_fineTuning2/model.h5")
-  
-    for image in os.listdir("phone"):
-        print(classification.classification(vgg_model, "phone/"+image))
-       
-        # if res != 0:
-        #     count = count +1
-        #     if count == 2:
-        #         flag = 1
-        # print(flag)
+
+    while(True):
+
+        retrieve, frames = mac_cam.read()
+        cv2.imshow('Try2Catch', frames)
+        count = count + 1
+        if count == 20:
+            
+            cv2.imwrite("image/"+str(num_image)+".png",frames)
+            count = 0
+            num_image = num_image + 1
+
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+        if len(os.listdir("image")) == 10:
+            for image in os.listdir("image"):
+                if classification.classification(vgg_model,"image/"+image) == "Phone":
+                   flag_phone = flag_phone + 1
+                elif classification.classification(vgg_model,"image/"+image) == "Distraction":
+                    flag_dist = flag_dist + 1
+                else:
+                    flag_safe = flag_safe + 1
+                os.remove("image/"+image)
+                num_image = 0
+     
+            if max(flag_phone, flag_dist, flag_safe) == flag_dist:
+                playsound('audio/dist.m4a')
+            elif max(flag_phone, flag_dist, flag_safe) == flag_phone:
+                playsound('audio/phone.m4a')
+            elif max(flag_phone, flag_dist, flag_safe) == flag_safe:
+                playsound('audio/safe.m4a')
+
+            flag_safe = 0
+            flag_phone = 0
+            flag_dist = 0
+    
  
